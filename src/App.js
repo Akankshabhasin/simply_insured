8105c7b99f0f94e487c24a226a299160edc44ee7
tranoidityFluidment <maintoabhinavgarg@gmail.com>
2018-08-12 02:42:58 +0530

    Fixed git issue of not pushing certain files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import Startup from './components/startup/startup'
+// import Startup from './components/startup/startup'
+import Credit from "./components/credit/credit";
+import Startup from "./components/startup/startup";
 import Dashboard from "./components/dashboard/dashboard";
+// import Dashboard from "./components/dashboard/dashboard";
+// import Credit from "./components/credit/credit"
 
 class App extends React.Component {
     render() {
         return (
-            <Startup/>
+            <Dashboard/>
         )
     }
 }
